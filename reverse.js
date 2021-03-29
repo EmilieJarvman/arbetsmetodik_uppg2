1b5225e0f8f2281bad7c18e431cd8269a7e8e0e1
Emilie <emilie@jarvman.com>
2021-03-29 21:49:48 +0200

    add function to reverse string

--- reverse.js
+++ reverse.js
@@ -1,2 +1,9 @@
 // -------- FUNCTION 4 --------
 // Function that will make a string the argument and return the reverse
+
+let reverse = (stringReverse) => stringReverse.split("").reverse().join("");
+ 
+console.log(reverse("word"));
+
+
+// module.exports = reverse;
